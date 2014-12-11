e466b883ca9556b80b1b1ce3ef5a7771c60cfce9
John W. Fail <fail@cenotaph.org>
2014-12-11 12:23:19 +0200

    gem fixes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Moks::Application.config.session_store :cookie_store, key: '_moks_session', domain: 'moks.ee'
+Moks::Application.config.session_store :cookie_store, key: '_moks_session', domain: :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
