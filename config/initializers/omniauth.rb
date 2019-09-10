9009939cb83cc594dc06da94f5f66693e5549553
John W. Fail <john@pixelache.ac>
2019-09-10 15:11:03 +0300

    update omni-auth facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret'], {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
+  provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret'] #, {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
   provider :twitter, ENV['twitter_consumer_key'], ENV['twitter_consumer_secret']
   provider :google_oauth2, ENV["google_client_id"], ENV["google_client_secret"], {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
 
