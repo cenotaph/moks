72dfd5f36af41268a9cab88922da97c0f7b0a3fe
John W. Fail <john@pixelache.ac>
2019-09-10 15:20:24 +0300

    trying again

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret'] #, {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
+  provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret'] , token_params: { parse: :json },
+    client_options: {
+      site: 'https://graph.facebook.com/v3.0',  # this is the example API version
+      authorize_url: "https://www.facebook.com/v3.0/dialog/oauth"
+    }#, {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
   provider :twitter, ENV['twitter_consumer_key'], ENV['twitter_consumer_secret']
   provider :google_oauth2, ENV["google_client_id"], ENV["google_client_secret"], {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
 
